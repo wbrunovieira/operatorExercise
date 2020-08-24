184f5ee0d8f12d399e0c95651599588ea0cc47b5
wbrunovieira <wbrunovieira77@gmail.com>
2020-08-24 20:40:42 +0100

    exercise finished

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remander {mod} ");
+
+
+            //Allow the user input the radius
+
+            Console.WriteLine("Give me a radius, I calculate the area for you !");
+            double realRadius;
+            var radius = double.TryParse(Console.ReadLine(), out realRadius);
+             
+            //Calculate area of the circle
+            var areaOfCircle = CalculateArea(realRadius); 
+
+            Console.WriteLine($"The area of circle with radius of {realRadius} is {areaOfCircle}");
+
+        }
+
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
+        }
+    }
+}
